3aa327374e2535a9437862c7994abdfd647750f5
Mateus Noleto <mateusnoletods@gmail.com>
2025-05-30 11:32:28 -0300

    Criado rotas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-const express = require('express')
+import * as express from 'express'
 const app = express ()
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('GET /')
   })
+
+app.post('/', (req, res) => {
+  res.status(201).send('POST /')
+})
+
+app.put('/', (req, res) => {
+  res.send('PUT /')
+})
+
+app.delete('/', (req, res) =>{
+  res.send('DElETE /')
+})
    
   app.listen(3000, () => console.log('ONLINE http://localhost:3000'))
